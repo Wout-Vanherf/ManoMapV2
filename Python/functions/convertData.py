268b5b85dd2caa47e1c26dff12916b513151ff41
thomas <thomas.vangansen@student.ucll.be>
2023-09-21 10:07:47 +0200

    comments engels en /10 function

--- Python/functions/convertData.py
+++ Python/functions/convertData.py
@@ -1,9 +1,16 @@
 def convertData(data):
-    #check of eerste entry van het fout format is (Name=...)
-    eerste_paar_letters = data[0][0][:len('Name=')]
-    if eerste_paar_letters == 'Name=':
-        #delete de eerste 7 lines
-        return data[7:]
+    #check if first entry is wrong format (Name=...)
+    try:
+        substr = data[0][0][:len('Name=')]
+    except:
+        substr = ''
+    if substr == 'Name=':
+        #delete first 7 lines
+        data = data[7:]
+        #divide first entry by 10 to get 1/10th of second
+        for line in data:
+            line[0] = line[0]/10
+        return data
     else:
         return data
 
@@ -18,7 +25,7 @@ if __name__ == '__main__':
     [5],
     [6],
     [7],
-    [8],
+    [8, 'random'],
     ]
 
     new_data = convertData(data)
